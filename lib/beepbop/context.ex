740e91e63395e13ac3d5dd33039de30d1498f86a
Ananya Bahadur <ananya95@gmail.com>
2018-06-25 11:53:18 +0530

    Add :errors key to Context

--- lib/beepbop/context.ex
+++ lib/beepbop/context.ex
@@ -3,7 +3,7 @@ defmodule BeepBop.Context do
 
   alias Ecto.Multi
 
-  defstruct ~w(struct state multi valid?)a
+  defstruct ~w(struct state multi valid? errors)a
 
   @typedoc """
   The Context struct
@@ -12,7 +12,8 @@ defmodule BeepBop.Context do
           struct: struct,
           state: map,
           multi: Multi.t(),
-          valid?: boolean
+          valid?: boolean,
+          errors: term
         }
 
   @spec new(struct, keyword) :: t
@@ -21,7 +22,8 @@ defmodule BeepBop.Context do
       struct: struct,
       state: Keyword.get(opts, :state, %{}),
       multi: Keyword.get(opts, :multi, Multi.new()),
-      valid?: Keyword.get(opts, :valid?, true)
+      valid?: Keyword.get(opts, :valid?, true),
+      errors: Keyword.get(opts, :errors)
     }
   end
 end
